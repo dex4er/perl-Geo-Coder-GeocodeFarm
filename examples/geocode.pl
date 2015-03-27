b8decc61d11c3b5d21bf223639c6e55e35eaa95e
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-27 18:23:25 +0100

    Bugfix for example scripts

--- examples/geocode.pl
+++ examples/geocode.pl
@@ -9,7 +9,7 @@ use Data::Dumper;
 my %args = map { /^(.*?)=(.*)$/ and ($1 => decode_utf8($2)) } @ARGV;
 
 die "Usage: geocode.pl key=3d517dd448a5ce1c2874637145fed69903bc252a location='530 West Main St Anoka MN 55303'\n"
-    unless defined $args{key} and defined $args{location};
+    unless defined $args{location} or defined $args{addr};
 
 my $geocoder = Geo::Coder::GeocodeFarm->new(%args);
 
--- examples/reverse_geocode.pl
+++ examples/reverse_geocode.pl
@@ -8,7 +8,7 @@ use Data::Dumper;
 my %args = map { /^(.*?)=(.*)$/ and ($1 => $2) } @ARGV;
 
 die "Usage: reverse_geocoder.pl key=3d517dd448a5ce1c2874637145fed69903bc252a latlng='45.2040305,-93.3995728'\n"
-    unless defined $args{key} and (defined $args{latlng} or defined $args{lat} and defined $args{lng});
+    unless defined $args{latlng} or defined $args{lat} and defined $args{lon};
 
 my $geocoder = Geo::Coder::GeocodeFarm->new(%args);
 
